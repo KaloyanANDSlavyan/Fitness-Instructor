f4e959289ae752d5d2e5fc43ac91ef52e3a1e885
slavyanHristov <slavqnhristov@gmail.com>
2021-04-24 20:30:45 +0300

    Setup of project complete
    1.Created Windows Forms project.
    2. Created basic design for the first form.
    3. Added Database to the project with Table 'Clients' which will contain info about the client
    4. Establised database connection.

---
+++ Fitness_Instructor/DatabaseAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+
+
+namespace Fitness_Instructor
+{
+    class DatabaseAccess
+    {
+        public static string CnnVal(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+    }
+}
---
+++ Fitness_Instructor/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fitness_Instructor
+{
+    class Client
+    {
+
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int Age { get; set; }
+        public float Height { get; set; }
+        public float Weight { get; set; }
+        private bool Gender { get; set; }
+    }
+}
