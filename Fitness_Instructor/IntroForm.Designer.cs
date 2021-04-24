f1c8cf417200051748cf28c25824b01b23801aa7
slavyanHristov <slavqnhristov@gmail.com>
2021-04-25 00:41:32 +0300

    Inital work on UI

--- Fitness_Instructor/IntroForm.Designer.cs
+++ Fitness_Instructor/IntroForm.Designer.cs
@@ -28,46 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(351, 143);
+            this.label1.Font = new System.Drawing.Font("Impact", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(303, 97);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.Size = new System.Drawing.Size(115, 98);
             this.label1.TabIndex = 0;
             this.label1.Text = "FS";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(199, 238);
+            this.progressBar1.Location = new System.Drawing.Point(110, 280);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(333, 29);
+            this.progressBar1.Size = new System.Drawing.Size(500, 13);
             this.progressBar1.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(256, 185);
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(246, 195);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(231, 20);
+            this.label2.Size = new System.Drawing.Size(232, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Created by: Slavyan and Radoslav";
             // 
-            // Form1
+            // timer1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // IntroForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
             this.ClientSize = new System.Drawing.Size(729, 493);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Name = "IntroForm";
             this.Text = "Fitness Instructor";
+            this.Load += new System.EventHandler(this.IntroForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,6 +93,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Fitness_Instructor/IntroForm.cs
+++ Fitness_Instructor/IntroForm.cs
@@ -12,11 +12,43 @@ namespace Fitness_Instructor
 {
     public partial class IntroForm : Form
     {
+
         public IntroForm()
         {
             InitializeComponent();
-            DatabaseAccess db = new DatabaseAccess();
+            
         }
 
+        private void IntroForm_Load(object sender, EventArgs e)
+        {
+           
+            DatabaseAccess db = new DatabaseAccess();
+            handleProgressBar();
+        }
+
+        private void handleProgressBar()
+        {
+            this.timer1.Enabled = true;
+            this.timer1.Start();
+            this.timer1.Interval = 100;
+            progressBar1.Maximum = 100;
+            timer1.Tick += new EventHandler(timer1_Tick);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (progressBar1.Value != 100)
+                progressBar1.Value++;
+            else
+            {
+                timer1.Stop();
+                Menu m = new Menu();
+                this.Hide();
+                m.ShowDialog();
+                this.Close();
+            }
+         
+        }
     }
 }
